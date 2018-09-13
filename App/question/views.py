3e3566111fbe8282c462afd274a2e8eda507cd74
Fiona K Owuor <fiona@localhost.localdomain>
2018-09-13 20:02:47 +0300

    ft:validate question #160250425

--- App/question/views.py
+++ App/question/views.py
@@ -6,17 +6,27 @@ from App import app
 api = Api(app)
 
 class question(Resource):
+
 	def post(self):
-		question = request.get_json()['question']
+		try:
+			question = request.get_json()['question'].strip()
+		except KeyError:
+			return jsonify({'message':'question must be provided'})
+		if len(question) == 0:
+			return jsonify({'message':'question can not be blank'})
 		quests.append(question)
 		return jsonify({"message":question})
-
+        
 	def get(self):
-		return jsonify(give_id(quests))
+		return jsonify(quest_id(quests))
 
 class questionId(Resource):
+
 	def get(self, id):
-		return jsonify(give_id(quests)[id])
+		if id not in quests:
+			return jsonify({'message':'question does not exeist'})
+		else:
+			return jsonify(quest_id(quests)[id])
 
 api.add_resource(question, '/stackoverflow.com/api/v1/question/')
 api.add_resource(questionId, '/stackoverflow.com/api/v1/questionId/<int:id>')				
