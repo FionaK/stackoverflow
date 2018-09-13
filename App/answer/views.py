b743ef8a2e3b27c20b4c823e72ac2a3f70617888
Fiona K Owuor <fiona@localhost.localdomain>
2018-09-13 20:04:21 +0300

    Post answer #160250514

--- App/answer/views.py
+++ App/answer/views.py
@@ -8,15 +8,17 @@ api = Api(app)
 class answer(Resource):
 	def post(self, id):
 		try:
-			answer = request.get_json()['answer']
+			answer = request.get_json()['answer'].strip()
 		except KeyError:
-			return jsonify({"message":"answer must be provided"})	
-		if id not in give_id(quests):
+			return jsonify({"message":"answer must be provided"})
+		if len(answer) == 0:
+		    return jsonify({'message':'Answer can not be blank'})	
+		if id not in quest_id(quests):
 			return jsonify({"message":"Can't post an answer,question does not exist"})
 		if id in ans:
 			ans[id].append(answer)
 		else:
 			ans.update({id:[answer]})
-		return jsonify({give_id(quests)[id]:ans[id]})
+		return jsonify({quest_id(quests)[id]:ans[id]})
 
-api.add_resource(answer, '/stackoverflow.com/api/v1/answer/<int:id>')		
+api.add_resource(answer, '/stackoverflow.com/api/v1/answer/<int:id>')
