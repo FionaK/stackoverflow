a3a4a1e854f802b2cede3f3be34453dfa87d37e5
Fiona K Owuor <fiona@localhost.localdomain>
2019-05-03 15:35:37 -0400

    Basic setup

--- trial/Trial/manage.py
+++ trial/Trial/manage.py
@@ -8,12 +8,19 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Trial.settings')
     try:
         from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+    except ImportError:
+        #THe above import my fail for some other reason. Ensure that the
+        #issue really is that django is missing to avoid making other
+        #exceptions in python 2.
+        try:
+            import django
+        except ImportError:
+            raise ImportError(
+                "Couldn't import Django. Are you sure it's installed and "
+                "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?"
+            )
+        raise
     execute_from_command_line(sys.argv)
 
 
